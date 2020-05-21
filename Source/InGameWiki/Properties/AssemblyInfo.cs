b21af95ac987923e745887628bae1e98c90f7501
James Billy <james@epicguru.co.uk>
2020-05-21 23:32:38 +0200

    Messing with NuGet packages.
    Ultimately, I plan to have this work the same way that Harmony does: mod developers add the NuGet package/dll, but don't copy to output. They then add a dependency on the mod, which loads the 'master' dll.
    Finally then the game loads, the master mod loads the master dll, the mods that add wiki's now have access to the API, and everyone is happy. If the master mod/API updates, the each mod author has to download the latest NuGet package and re-publish their mod to fix any conflicts.

--- Source/InGameWiki/Properties/AssemblyInfo.cs
+++ Source/InGameWiki/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("In-Game Wiki")]
-[assembly: AssemblyDescription("Lib to add support for the in-game wiki mod, for Rimworld.")]
+[assembly: AssemblyDescription("Library to add support for the in-game wiki mod.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Epicguru")]
 [assembly: AssemblyProduct("InGameWiki")]

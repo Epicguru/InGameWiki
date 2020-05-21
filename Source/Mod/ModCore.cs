d447c9197eb1fa8d9f8545f32627dd790c4d7672
James Billy <james@epicguru.co.uk>
2020-05-21 23:04:30 +0200

    Initial content.

---
+++ Source/Mod/ModCore.cs
@@ -0,0 +1,12 @@
+﻿using Verse;
+
+namespace Mod
+{
+    public class ModCore : Verse.Mod
+    {
+        public ModCore(ModContentPack content) : base(content)
+        {
+            Log.Message("<color=cyan>Loaded in-game wiki mod.</color>");
+        }
+    }
+}
